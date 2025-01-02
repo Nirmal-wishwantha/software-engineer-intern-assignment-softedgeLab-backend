a6454c2fd22dd57e211fd6334bfb4e168df93b3b
Nirmal-wishwantha <nirmalwishwantha@gmail.com>
2025-01-02 23:57:20 +0530

    create controller class

---
+++ src/main/java/com/softedgelabs/assignment/controller/AttendeesController.java
@@ -0,0 +1,9 @@
+package com.softedgelabs.assignment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Attendees")
+public class AttendeesController {
+}
